99959ab60d41a1683e7634031fc070c7ed86650f
Woojung618 <eyes_cat@naver.com>
2019-11-07 16:02:54 +0900

    5-2

--- main.c
+++ main.c
@@ -5,17 +5,17 @@
 
 int main(void){
 	int i;
-	//int sum=0;
+	int sum=0;
 	int grade[5];
 	for (i=0;i<5;i++){
 		printf("input value (%i) : ",i);
 		scanf("%d",&grade[i]);
-		//sum += grade[i];
+		sum += grade[i];
 	}
 	for (i=0;i<5;i++)
 		printf("grade[%d] = %d\n",i,grade[i]);
 		
-	//printf("\n\naverage : %d",sum/5);
+	printf("\n\naverage : %d",sum/5);
 	return 0;
 }
 
