601cbcd52d14764b97e10a3ce73f0f122fac0a95
Woojung618 <eyes_cat@naver.com>
2019-11-07 15:14:59 +0900

    2

--- main.c
+++ main.c
@@ -5,10 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	int i=10;
-	char c=69;
-	float f=12.3;
-	printf("i: %i %p \n",&i,&i);
-	printf("c: %i %p \n",&c,&c);
-	printf("f: %i %p \n",&f,&f);
+	char c='a';
+	int * iptr;
+	iptr = &i;
+	char * cptr;
+	cptr = &c;
+	int * iptr2;
+	iptr2=iptr;
+	printf("i: %p\n%p (size:%i)\n",iptr,&i,sizeof(iptr));
+	printf("c: %p\n%p (size:%i)\n",cptr,&c,sizeof(cptr));
+	printf("iptr: %p, %i\n",iptr2,*iptr2);
 	return 0;
 }
